8129e3a48b274cd50b92dbab4cc40a099ceaf748
Karl Eklund <localpart@gmail.com>
2021-06-27 23:09:14 +0200

    mix phx.new --no-ecto . --live --verbose --app shopping_list

---
+++ lib/shopping_list/application.ex
@@ -0,0 +1,32 @@
+defmodule ShoppingList.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      ShoppingListWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: ShoppingList.PubSub},
+      # Start the Endpoint (http/https)
+      ShoppingListWeb.Endpoint
+      # Start a worker by calling: ShoppingList.Worker.start_link(arg)
+      # {ShoppingList.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ShoppingList.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ShoppingListWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
