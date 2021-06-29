33051c23e60ffb26429c17c8ccf1a8a73badd70b
Karl Eklund <localpart@gmail.com>
2021-06-29 11:54:08 +0200

    Start server

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :shopping_list, ShoppingListWeb.Endpoint,
   url: [host: "example.com", port: System.fetch_env!("PORT")],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
