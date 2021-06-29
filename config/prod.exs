2f6051102a5aafd8d2954227caafc6662e09a217
Karl Eklund <localpart@gmail.com>
2021-06-29 15:58:49 +0200

    Change to port 443

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :shopping_list, ShoppingListWeb.Endpoint,
-  url: [host: System.fetch_env!("WEB_HOST"), port: 80],
+  url: [host: System.fetch_env!("WEB_HOST"), port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
